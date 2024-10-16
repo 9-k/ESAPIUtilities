7558e39bd0c6fbab9a00f446960c286db81d0678
9K <schrian25@gmail.com>
2024-10-16 13:59:18 -0500

    Added some whatnot for minimum structure finding.

--- ESAPIUtilities/ESAPIUtility.cs
+++ ESAPIUtilities/ESAPIUtility.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Windows.Media;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using VMS.TPS.Common.Model.API;
 using VMS.TPS.Common.Model.Types;
 using Microsoft.VisualBasic;
+using System.Windows.Media.Media3D;
 
 namespace ESAPIUtilities
 {
@@ -257,6 +259,23 @@ namespace ESAPIUtilities
                 throw new Exception(message);
             }
             return verificationPlan;
+        }
+
+        public static double MinimumStructureDistance(Structure firstStructure, Structure secondStructure)
+        {
+            double minDistance = double.MaxValue;
+            foreach (var firstStructureSurfacePoint in firstStructure.MeshGeometry.Positions)
+            {
+                foreach (var secondStructureSurfacePoint in secondStructure.MeshGeometry.Positions)
+                {
+                    double dist = Point3D.Subtract(firstStructureSurfacePoint, secondStructureSurfacePoint).Length;
+                    if (dist < minDistance)
+                    {
+                        minDistance = dist;
+                    }
+                }
+            }
+            return minDistance;
         }
     }
 }
